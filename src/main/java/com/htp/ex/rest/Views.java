11fbdf0af4f1745596198b08f0f837ce29a3adb2
Vladislav Bedritsky <vladquinn2010@gmail.com>
2020-01-12 14:06:03 +0300

    bugs fixed + JsonView

---
+++ src/main/java/com/htp/ex/rest/Views.java
@@ -0,0 +1,10 @@
+package com.htp.ex.rest;
+
+public final class Views {
+
+    public interface Id {}
+
+    public interface IdAndName extends Id {}
+
+    public interface IdAndLocalDate extends Id {}
+}
