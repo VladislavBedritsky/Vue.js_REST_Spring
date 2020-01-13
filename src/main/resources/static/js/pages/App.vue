8d3d0515c329fea3369ddd772d98b9b030e3b4e2
Vladislav Bedritsky <vladquinn2010@gmail.com>
2020-01-13 19:07:46 +0300

    add webpack, babel ,vue dependencies by yarn CLI

---
+++ src/main/resources/static/js/pages/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+    <div v-if="!profile">Необходимо авторизоваться через
+        <a href="/login">Google</a>
+    </div>
+    <div v-else>
+       <div>
+            {{profile.name}} <a href="/logout">Выйти</a>
+            </div>
+<!--        <messages-list v-bind:mes="messages" />-->
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+          data: function() {
+            return {
+              messages: frontendData.messages,
+              profile: frontendData.profile
+            }
+          }
+    }
+</script>
+
+<style>
+
+</style>
